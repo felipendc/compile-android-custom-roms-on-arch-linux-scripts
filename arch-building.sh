22dbb900c5f4d9401068632d476923c2c757fa79
Alexandre Rangel <mamutal91@gmail.com>
2019-03-07 17:42:02 -0300

    Arch Linux build preparation script
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

---
+++ arch-building.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# github.com/mamutal91
+
+# rm -rf arch-building.sh.sh && wget https://raw.githubusercontent.com/mamutal91/scripts/master/arch-building.sh && sudo chmod +x arch-building.sh && ./arch-building.sh
+
+export TERM=xterm-256color
+
+sudo pacman -S reflector --noconfirm
+sudo reflector -l 10 --sort rate --save /etc/pacman.d/mirrorlist
+
+function pacman(){
+  sudo pacman -Sy base-devel go git zsh zsh-syntax-highlighting repo wget ccache imagemagick cronie ntp lzip --needed --noconfirm
+}
+
+function yay-install(){
+  rm -rf yay
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay
+}
+
+readonly PACMAN_CLOUD=(
+  lib32-ncurses lib32-libxml2 lib32-gconf lib32-libcroco lib32-libsoup lib32-llvm-libs lib32-readline lib32-js17 lib32-librsvg lib32-mesa lib32-rest lib32-cairo
+  lib32-glew1.10 lib32-gtk3 lib32-polkit lib32-colord lib32-gtk2 lib32-pango lib32-libdbusmenu-gtk2 lib32-libindicator-gtk2 lib32-libappindicator-gtk2)
+
+readonly AUR_CLOUD=(
+  gnupg flex bison gperf sdl wxgtk squashfs-tools curl ncurses zlib schedtool perl-switch zip unzip libxslt python2-virtualenv bc gcc-multilib lib32-zlib
+  rsync lib32-ncurses5-compat-libs lib32-readline make pngcrush pngquant)
+
+function pacman-cloud(){
+  for i in "${PACMAN_CLOUD[@]}"; do
+    sudo pacman -S ${i} --needed --noconfirm
+  done
+}
+
+function aur-cloud(){
+  for i in "${AUR_CLOUD[@]}"; do
+    yay -S ${i} --needed --noconfirm
+  done
+}
+
+function bin(){
+  rm -rf ~/.bin
+  mkdir ~/.bin
+  PATH=~/.bin:$PATH
+  curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+  sudo chmod a+x ~/.bin/repo
+}
+
+function symlinksdk(){
+  rm -rf android* .android*
+  wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+  tar -xvzf android-sdk_r24.4.1-linux.tgz
+  mv android-sdk-linux .android-sdk
+  rm -rf android-sdk_r24.4.1-linux.tgz
+  ln -s /usr/bin/make-3.81 ~/.bin/make
+  ln -s /usr/bin/make-3.81-config ~/.bin/make-config
+  ln -s /usr/bin/python2 ~/.bin/python
+  ln -s /usr/bin/python2-config ~/.bin/python-config
+  gpg --keyserver pgp.mit.edu --recv-keys C52048C0C0748FEE227D47A2702353E0F7E48EDB
+  yay -S ncurses5-compat-libs --needed --noconfirm
+}
+
+function java(){
+  yay -S jdk8-openjdk --needed --noconfirm
+  sudo archlinux-java set java-8-openjdk
+}
+
+function hour(){
+  sudo ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+}
+
+function config_system(){
+  sudo systemctl enable cronie
+  sudo systemctl enable ntpd
+  sudo chown -R $USER:$USER /home/$USER
+  chmod +x /home/$USER
+}
+
+function zsh(){
+  rm -rf $HOME/.oh-my-zsh
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  rm -rf .zshrc
+  wget https://raw.githubusercontent.com/mamutal91/buildroid/master/home/.zshrc
+}
+
+pacman
+yay-install
+pacman-cloud
+aur-cloud
+bin
+symlinksdk
+java
+hour
+config_system
+zsh
+
+#rm -rf android*
