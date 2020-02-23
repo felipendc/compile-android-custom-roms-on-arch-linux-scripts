9b2d0ae0480cb5c8e67816ed912224fa13396398
felipendc <felipendc10@gmail.com>
2020-02-23 01:25:41 -0300

    update

---
+++ tested-on-vicyoslinux/oneplus3-device-vendor-kernel-tree.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/felipendc/android_device_oneplus_oneplus3.git -b lineage-17.1 device/oneplus/oneplus3
+git clone https://github.com/felipendc/proprietary_vendor_oneplus.git -b lineage-17.1 vendor/oneplus
+git clone https://github.com/felipendc/android_kernel_oneplus_msm8996.git -b lineage-17.1 kernel/oneplus/msm8996
+
