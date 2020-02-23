d4a66cfb027b02d2639b7b6986213b527e57d23f
felipendc <felipendc10@gmail.com>
2020-02-23 01:57:45 -0300

    update

--- tested-on-vicyoslinux/oneplus3-device-vendor-kernel-tree.sh
+++ tested-on-vicyoslinux/oneplus3-device-vendor-kernel-tree.sh
@@ -5,6 +5,8 @@
 # My personal Linux Distribution: VicyosLinux (Based on Arch)
 # GDRIVE: https://drive.google.com/drive/folders/1JyI8O4DhJb9ktIaffiJYu966r9OIUwQI
 # Or, if you will... you can also build it from the source: https://github.com/felipendc/vicyos-build
+
+
 git clone https://github.com/felipendc/android_device_oneplus_oneplus3.git -b lineage-17.1 device/oneplus/oneplus3
 git clone https://github.com/felipendc/proprietary_vendor_oneplus.git -b lineage-17.1 vendor/oneplus
 git clone https://github.com/felipendc/android_kernel_oneplus_msm8996.git -b lineage-17.1 kernel/oneplus/msm8996
