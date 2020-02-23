772b5a3f548a168e95d27c56b9622e2309421942
felipendc <felipendc10@gmail.com>
2020-02-23 04:07:43 -0300

    update

--- tested-on-vicyoslinux/oneplus3-device-vendor-kernel-tree.sh
+++ tested-on-vicyoslinux/oneplus3-device-vendor-kernel-tree.sh
@@ -9,6 +9,20 @@
 
 
 git clone https://github.com/felipendc/android_device_oneplus_oneplus3.git -b lineage-17.1 device/oneplus/oneplus3
+git clone https://github.com/felipendc/android_device_oppo_common.git -b lineage-17.1 device/oppo/common
 git clone https://github.com/felipendc/proprietary_vendor_oneplus.git -b lineage-17.1 vendor/oneplus
 git clone https://github.com/felipendc/android_kernel_oneplus_msm8996.git -b lineage-17.1 kernel/oneplus/msm8996
+
+###############################################################
+#################### FOR AOSP CUSTOM ROMS  ####################
+###############################################################
+# . build/envsetup.sh or  source build/envsetup.sh
+# lunch aosp_oneplus3
+# make -j$(nproc --all) bacon
+# For detailed logs, use:
+# make -j$(nproc --all) bacon 2>&1 | tee log.txt
+# Remember to make clobber && make clean every now and then!
+###############################################################
+
+ 
 
