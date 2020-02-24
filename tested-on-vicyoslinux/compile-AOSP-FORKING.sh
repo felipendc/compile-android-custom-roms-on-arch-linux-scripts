57bd272c9256a43741a174b405dc848685dd5c4a
felipendc <felipendc10@gmail.com>
2020-02-23 22:11:48 -0300

    Testing functions.

--- tested-on-vicyoslinux/compile-AOSP-FORKING.sh
+++ tested-on-vicyoslinux/compile-AOSP-FORKING.sh
@@ -8,15 +8,22 @@
 
 
 
-aosp_dir="$HOME/AOSP-FORKING"
+export aosp_dir="$HOME/AOSP-FORKING"
 
-cd $aosp_dir/repo sync --force-sync && opengapps
 
-cd $aosp_dir/vendor/opengapps/build && git lfs fetch --all && git lfs pull
-cd $aosp_dir/vendor/opengapps/sources/all && git lfs fetch --all && git lfs pull
-cd $aosp_dir/vendor/opengapps/sources/arm && git lfs fetch --all && git lfs pull
-cd $aosp_dir/vendor/opengapps/sources/arm64 && git lfs fetch --all && git lfs pull
-cd $aosp_dir/vendor/opengapps/sources/x86 && git lfs fetch --all && git lfs pull
-cd $aosp_dir/vendor/opengapps/sources/x86_64 && git lfs fetch --all && git lfs pull
+function sync () {
+    cd $aosp_dir/repo sync --force-sync && opengapps
+}
 
-cd $aosp_dir/make clobber && make clean && . build/envsetup.sh && lunch aosp_oneplus3-userdebug && make -j$(nproc --all) bacon 2>&1 | tee log.txt
+function lfs () {
+    cd $aosp_dir/vendor/opengapps/build && git lfs fetch --all && git lfs pull
+    cd $aosp_dir/vendor/opengapps/sources/all && git lfs fetch --all && git lfs pull
+    cd $aosp_dir/vendor/opengapps/sources/arm && git lfs fetch --all && git lfs pull
+    cd $aosp_dir/vendor/opengapps/sources/arm64 && git lfs fetch --all && git lfs pull
+    cd $aosp_dir/vendor/opengapps/sources/x86 && git lfs fetch --all && git lfs pull
+    cd $aosp_dir/vendor/opengapps/sources/x86_64 && git lfs fetch --all && git lfs pull
+}
+
+function lfs () {
+    cd $aosp_dir/make clobber && make clean && . build/envsetup.sh && lunch aosp_oneplus3-userdebug && make -j$(nproc --all) bacon 2>&1 | tee log.txt
+}
