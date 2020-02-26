0e4998520da6af7c3ffe643e2ad17a9354f7bd20
felipendc <felipendc10@gmail.com>
2020-02-26 01:37:12 -0300

    beryllium kernel and     sudo rm -r device/xiaomi/beryllium*

--- tested-on-vicyoslinux/beryllium/compile-AOSP-FORKING-beryllium.sh
+++ tested-on-vicyoslinux/beryllium/compile-AOSP-FORKING-beryllium.sh
@@ -15,6 +15,15 @@ function sync () {
     sudo rm -r device/xiaomi/beryllium*
     git clone https://github.com/mamutal91/device_xiaomi_beryllium -b ten device/xiaomi/beryllium
     sudo rm -r device/xiaomi/beryllium/.git
+    sudo rm -r device/xiaomi/sdm845-common*
+    git clone https://github.com/mamutal91/device_xiaomi_sdm845-common -b ten device/xiaomi/sdm845-common
+    sudo rm -r device/xiaomi/sdm845-common/.git
+    sudo rm -r kernel/xiaomi/sdm845*
+    git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-17.1 kernel/xiaomi/sdm845
+    sudo rm -r kernel/xiaomi/sdm845/.git
+#    sudo rm -r kernel/xiaomi/sdm845*
+#    git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-17.0 kernel/xiaomi/sdm845
+#    sudo rm -r kernel/xiaomi/sdm845/.git
     repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
 }
 
