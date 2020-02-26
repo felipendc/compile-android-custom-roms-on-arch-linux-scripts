8d83b781cdca754f3d72f3a8dd18e8c1ffd99fa2
felipendc <felipendc10@gmail.com>
2020-02-26 10:02:24 -0300

    beryllium recompile script

--- tested-on-vicyoslinux/beryllium/recompile-AOSP-FORKING-beryllium.sh
+++ tested-on-vicyoslinux/beryllium/recompile-AOSP-FORKING-beryllium.sh
@@ -9,25 +9,10 @@
 
 export aosp_dir="$HOME/AOSP-FORKING"
 
-function sync () {
-    cd $aosp_dir
-    git lfs install
-    sudo rm -r device/xiaomi/beryllium*
-    git clone https://github.com/felipendc/device_xiaomi_beryllium -b ten device/xiaomi/beryllium
-    sudo rm -r device/xiaomi/beryllium/.git
-    sudo rm -r device/xiaomi/sdm845-common*
-    git clone https://github.com/felipendc/android_device_xiaomi_sdm845-common -b lineage-17.1 device/xiaomi/sdm845-common
-    sudo rm -r device/xiaomi/sdm845-common/.git
-    sudo rm -r kernel/xiaomi/sdm845*
-    git clone https://github.com/felipendc/kernel_xiaomi_sdm845 -b ten kernel/xiaomi/sdm845
-    sudo rm -r kernel/xiaomi/sdm845/.git
-    sudo rm -r vendor/xiaomi*
-    git clone https://github.com/felipendc/proprietary_vendor_xiaomi -b ten vendor/xiaomi
-    sudo rm -r vendor/xiaomi/.git
-    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
-}
+
 
 function lfs () {
+    git lfs install
     cd $aosp_dir/vendor/opengapps/build && git lfs fetch --all && git lfs pull
     cd $aosp_dir/vendor/opengapps/sources/all && git lfs fetch --all && git lfs pull
     cd $aosp_dir/vendor/opengapps/sources/arm && git lfs fetch --all && git lfs pull
@@ -41,7 +26,5 @@ function compile () {
     make clobber && make clean && . build/envsetup.sh && lunch aosp_beryllium-userdebug && make -j$(nproc --all) bacon 2>&1 | tee log.txt
 }
 
-sync
 lfs
 compile
-
