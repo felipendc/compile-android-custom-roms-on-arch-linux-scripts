268070aaf0af74cc48fda1a193a5eff141e431a8
felipendc <felipendc10@gmail.com>
2020-02-26 09:30:26 -0300

    Beryllium AOSP_FORKING scripts

--- tested-on-vicyoslinux/beryllium/beryllium_device_vendor_kernel_tree_aosp_forking.sh
+++ tested-on-vicyoslinux/beryllium/beryllium_device_vendor_kernel_tree_aosp_forking.sh
@@ -11,17 +11,18 @@ export aosp_dir="$HOME/AOSP-FORKING"
 function sync () {
     cd $aosp_dir
     sudo rm -r device/xiaomi/beryllium*
-    git clone https://github.com/mamutal91/device_xiaomi_beryllium -b ten device/xiaomi/beryllium
+    git clone https://github.com/felipendc/device_xiaomi_beryllium -b ten device/xiaomi/beryllium
     sudo rm -r device/xiaomi/beryllium/.git
     sudo rm -r device/xiaomi/sdm845-common*
-    git clone https://github.com/mamutal91/device_xiaomi_sdm845-common -b ten device/xiaomi/sdm845-common
+    git clone https://github.com/felipendc/android_device_xiaomi_sdm845-common -b lineage-17.1 device/xiaomi/sdm845-common
     sudo rm -r device/xiaomi/sdm845-common/.git
     sudo rm -r kernel/xiaomi/sdm845*
-    git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-17.1 kernel/xiaomi/sdm845
+    git clone https://github.com/felipendc/kernel_xiaomi_sdm845 -b ten kernel/xiaomi/sdm845
     sudo rm -r kernel/xiaomi/sdm845/.git
-#    sudo rm -r kernel/xiaomi/sdm845*
-#    git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-17.0 kernel/xiaomi/sdm845
-#    sudo rm -r kernel/xiaomi/sdm845/.git
+    sudo rm -r vendor/xiaomi*
+    git clone https://github.com/felipendc/proprietary_vendor_xiaomi -b ten vendor/xiaomi
+    sudo rm -r vendor/xiaomi/.git
+    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
 }
 
 sync
