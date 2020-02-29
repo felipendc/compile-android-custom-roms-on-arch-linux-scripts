4b0b91e53528e588fb39d8612677dcd241a702ae
felipendc <felipendc10@gmail.com>
2020-02-29 13:37:43 -0300

    make clobber && make clean && . build/envsetup.sh && lunch aosp_raphael-userdebug && make -j$(nproc --all) bacon 2>&1 | tee log.txt

--- TESTED-ON-VICYOSLINUX/raphael(k20-pro)/AOSP_FORKING/compile-AOSP-FORKING-raphael.sh
+++ TESTED-ON-VICYOSLINUX/raphael(k20-pro)/AOSP_FORKING/compile-AOSP-FORKING-raphael.sh
@@ -6,6 +6,13 @@
 ###############################################################################################################
 ################## MAKE SURE TO MAKE THIS FILE EXECUTABLE TO PREVENT ANY PERMISSIONS ERRORS  ##################
 ###############################################################################################################
+#    sudo rm -r kernel/xiaomi/sm8150*
+#    git clone https://github.com/felipendc/kernel_xiaomi_sm8150 -b lineage-17.1 kernel/xiaomi/sm8150
+#    sudo rm -r kernel/xiaomi/sm8150/.git
+
+#    sudo rm -r kernel/xiaomi/sm8150*
+#    git clone https://github.com/UtsavBalar1231/kernel_xiaomi_raphael -b master kernel/xiaomi/sm8150
+#    sudo rm -r kernel/xiaomi/sm8150/.git
 
 export aosp_dir="$HOME/AOSP-FORKING"
 
@@ -13,19 +20,19 @@ function sync () {
     cd $aosp_dir
     git lfs install
 
-    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
-
     sudo rm -r device/xiaomi/raphael*
-    git clone https://github.com/felipendc/aosp_xiaomi_raphael -b ten device/xiaomi/raphael
+    git clone https://github.com/felipendc/device_xiaomi_raphael -b ten device/xiaomi/raphael
     sudo rm -r device/xiaomi/raphael/.git
 
-    sudo rm -r vendor/xiaomi/raphael*
-    git clone https://github.com/felipendc/proprietary_vendor_xiaomi -b ten vendor/xiaomi
+    sudo rm -r vendor/xiaomi*
+    git clone https://github.com/felipendc/vendor_xiaomi_raphael -b ten vendor/xiaomi
     sudo rm -r vendor/xiaomi/raphael/.git
 
     sudo rm -r kernel/xiaomi/sm8150*
-    git clone https://github.com/felipendc/kernel_xiaomi_sm8150 -b lineage-17.1 kernel/xiaomi/sm8150
+    git clone https://github.com/felipendc/kernel_xiaomi_raphael -b ten kernel/xiaomi/raphael
     sudo rm -r kernel/xiaomi/sm8150/.git
+
+    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
 }
 
 function lfs () {
--- TESTED-ON-VICYOSLINUX/raphael(k20-pro)/AOSP_FORKING/raphael_device_vendor_kernel_tree_aosp_forking.sh
+++ TESTED-ON-VICYOSLINUX/raphael(k20-pro)/AOSP_FORKING/raphael_device_vendor_kernel_tree_aosp_forking.sh
@@ -11,18 +11,19 @@ export aosp_dir="$HOME/AOSP-FORKING"
 
 function sync () {
     cd $aosp_dir
-    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
-    
     sudo rm -r device/xiaomi/raphael*
-    git clone https://github.com/felipendc/aosp_xiaomi_raphael -b ten device/xiaomi/raphael
+    git clone https://github.com/felipendc/device_xiaomi_raphael -b ten device/xiaomi/raphael
     sudo rm -r device/xiaomi/raphael/.git
 
-    sudo rm -r vendor/xiaomi/raphael*
-    git clone https://github.com/felipendc/proprietary_vendor_xiaomi -b ten vendor/xiaomi
+    sudo rm -r vendor/xiaomi*
+    git clone https://github.com/nullxception/vendor_xiaomi_raphael -b ten vendor/xiaomi
     sudo rm -r vendor/xiaomi/raphael/.git
 
     sudo rm -r kernel/xiaomi/sm8150*
-    git clone https://github.com/felipendc/kernel_xiaomi_sm8150 -b lineage-17.1 kernel/xiaomi/sm8150
+    git clone https://github.com/felipendc/kernel_xiaomi_raphael -b ten kernel/xiaomi/raphael
+    sudo rm -r kernel/xiaomi/sm8150/.git
+
+    repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync && opengapps
 }
 
 sync
