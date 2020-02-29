5ebbbcba8b0519c8b0047145e247588cb5e6b605
felipendc <felipendc10@gmail.com>
2020-02-29 13:44:50 -0300

    TitaniumOS BringUp Script Update

---
+++ TESTED-ON-VICYOSLINUX/oneplus3/TitaniumOS/device-vendor-kernel-titanium-oneplus3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Felipe Ndc "github.com/felipendc" 
+
+###############################################################################################################
+################## MAKE SURE TO MAKE THIS FILE EXECUTABLE TO PREVENT ANY PERMISSIONS ERRORS  ##################
+###############################################################################################################
+
+export titanium="$HOME/titanium"
+
+function sync () {
+    cd $titanium
+    
+    sudo rm -r device/oneplus/oneplus3*
+    git clone https://github.com/felipendc/titanium_device_oneplus_oneplus3 -b lineage-17.1 device/oneplus/oneplus3
+    sudo rm -r device/oneplus/oneplus3/.git
+
+    sudo rm -r device/oppo/common*
+    git clone https://github.com/felipendc/android_device_oppo_common.git -b lineage-17.1 device/oppo/common
+    sudo rm -r device/oppo/common/.git
+
+    sudo rm -r vendor/oneplus*
+    git clone https://github.com/felipendc/proprietary_vendor_oneplus.git -b lineage-17.1 vendor/oneplus
+    sudo rm -r vendor/oneplus/.git
+
+    sudo rm -r kernel/oneplus/msm8996*
+    git clone https://github.com/felipendc/android_kernel_oneplus_msm8996.git -b lineage-17.1 kernel/oneplus/msm8996
+    sudo rm -r kernel/oneplus/msm8996/.git
+
+    make clobber && make clean && . build/envsetup.sh && lunch titanium_oneplus3-userdebug && make -j$(nproc --all) titanium 2>&1 | tee log.txt
+}
+
+sync
+
